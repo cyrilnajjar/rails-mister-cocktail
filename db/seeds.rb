926ae077a32a0c0a87d1dc6fed7d21a76a639ca6
cyril najjar <najjarcyril@gmail.com>
2019-11-17 23:02:18 +0000

    Playing with the app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Cocktail.destroy_all
-Ingredient.destroy_all
-
-superwow = Cocktail.create(name: "Super Wow")
-bigdeal = Cocktail.create(name: "Big Deal")
-amazingdrink = Cocktail.create(name: "Amazing Drink")
-
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
